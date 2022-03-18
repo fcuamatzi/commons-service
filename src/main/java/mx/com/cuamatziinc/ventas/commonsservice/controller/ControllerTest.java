15ddbe4c7f94fd52874fce976963415c0b449096
fjavier <fjavier.cuamatzi@gmail.com>
2022-03-18 00:23:02 -0600

    se agrega path

--- src/main/java/mx/com/cuamatziinc/ventas/commonsservice/controller/ControllerTest.java
+++ src/main/java/mx/com/cuamatziinc/ventas/commonsservice/controller/ControllerTest.java
@@ -1,11 +1,13 @@
 package mx.com.cuamatziinc.ventas.commonsservice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
-	
+
+    @GetMapping("/valor")
 	public ResponseEntity<?> saludar(){
 		return ResponseEntity.ok().body("Hola mundo");
 	}

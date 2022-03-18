472fa7b82d9ddb7ab0c84838e28a0b35b1875fd5
fcuamatzi <fjavier.cuamatzi@gmail.com>
2022-03-18 00:12:36 -0600

    se agrego end point

--- src/main/java/mx/com/cuamatziinc/ventas/commonsservice/controller/ControllerTest.java
+++ src/main/java/mx/com/cuamatziinc/ventas/commonsservice/controller/ControllerTest.java
@@ -1,8 +1,13 @@
 package mx.com.cuamatziinc.ventas.commonsservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerTest {
+	
+	public ResponseEntity<?> saludar(){
+		return ResponseEntity.ok().body("Hola mundo");
+	}
 
 }
